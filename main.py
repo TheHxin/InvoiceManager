24d6b1b4770674e5603228c1ab2e58efea98e43b
deb <hxin.radin@gmail.com>
2025-04-03 11:26:58 +0200

    created post user login full functionality -> todo: get login

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Response, Cookie
 from pydantic import BaseModel, field_validator
 from datetime import datetime
 import json
+import bcrypt
+import secrets
 
  
 
@@ -19,6 +21,9 @@ class DataBase():
     
     def readInvoices(self):
         return self.readDB()["Invoices"]
+
+    def readUsers(self):
+        return self.readDB()["Users"]
     
     def writeAccount(self, account : "Account"):
         data = self.readDB()
@@ -32,11 +37,25 @@ class DataBase():
         with open(self.db_path,"w") as file:
             json.dump(data,file,indent=4)
 
+    def writeUser(self, user : "User"):
+        data = self.readDB()
+        data["Users"].append(json.loads(user.model_dump_json()))
+        with open(self.db_path,"w") as file:
+            json.dump(data,file,indent=4)
 
+    
 
 app = FastAPI()
 db = DataBase("./db.json")
 accounts_list = db.readAccounts()
+logged_in : list["UserLogin"] = []
+
+def genSessionKey() -> str:
+    return secrets.token_hex(16)
+def hashPassword(password) -> str:
+    return bcrypt.hashpw(password, bcrypt.gensalt())
+def checkPassword(password_plain, password_hashed) -> bool:
+    return bcrypt.checkpw(password_plain, password_hashed)
 
 class Account(BaseModel):
     name : str
@@ -53,6 +72,17 @@ class Invoice(BaseModel):
         if json.loads(v.model_dump_json()) not in accounts_list:
             raise ValueError("Account does not exist")
         return v
+    
+class User(BaseModel):
+    username : str
+    password : str
+
+class UserLogin(User):
+    session_key : str
+
+    def __init__(self, key):
+        self.session_key = key
+
 
 
 @app.get("/")
@@ -60,12 +90,16 @@ def index():
     return "service is started"
 
 @app.get("/invoices")
-def getInvoices() -> list[Invoice]:
+def get_invoices() -> list[Invoice]:
     return db.readInvoices()
 
 @app.get("/accounts")
 def get_account() -> list[Account]:
     return db.readAccounts()
+
+@app.get("/login")
+def get_login(resp = Response):
+    ...
 
 @app.post("/invoices")
 def post_invoices(invoice : Invoice) -> Invoice:
@@ -77,3 +111,16 @@ def post_acccount(account : Account) -> Account:
     db.writeAccount(account)
     accounts_list.append(json.loads(account.model_dump_json()))
     return account
+
+@app.post("/login")
+def post_login(user : User, resp : Response) -> bool:
+    users = db.readUsers()
+    for i in users:
+        if i["username"] == user.username:
+            if checkPassword(user.password, i["password"]):
+                session_key = genSessionKey()
+                logged_in.append(UserLogin(session_key))
+                resp.set_cookie(key="session_key",value=session_key,httponly=True,max_age=3600)
+                return True
+    return False
+                
